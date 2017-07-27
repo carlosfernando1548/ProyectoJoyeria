93f9fea33c7e751ac87478e5a8443601290228d3
Carlos Morales <carlosm3415@gmail.com>
2017-07-27 11:40:12 -0600

    reparado carrito

--- routes/web.php
+++ routes/web.php
@@ -65,6 +65,7 @@ Route::post('carrito/getCarrito', "CarritoController@getCarrito");
 Route::post('carrito/addProdCarrito', "CarritoController@addCarrito");
 Route::post('carrito/addFavorito', "CarritoController@addFavorito");
 Route::get('carrito/verCarrito', "CarritoController@verCarrito");
+Route::get('carrito/deletePro', "CarritoController@deletePro");
 Route::get('clientes/perfil', "UsuariosController@perfil");
 Route::post('pagos/pagar', "PagosController@pagar");
 
